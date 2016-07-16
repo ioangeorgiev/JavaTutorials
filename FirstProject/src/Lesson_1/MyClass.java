0b92f8ea03ba561e57a814fb9da589e77d363490
Йоан Георгиев <karattekid12@abv.bg>
2016-07-16 16:12:03 +0300

    This is my first java project

---
+++ FirstProject/src/Lesson_1/MyClass.java
@@ -0,0 +1,10 @@
+package Lesson_1;
+
+public class MyClass {
+	// This is my first Java project
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first Java project!");
+	}
+
+}
